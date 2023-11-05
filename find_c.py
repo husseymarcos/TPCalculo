185470cc5141670ab1c22b402f189b6f844524ea
husseymarcos <mhussey@mail.austral.edu.ar>
2023-11-05 17:43:24 -0300

    correcion datos

--- find_c.py
+++ find_c.py
@@ -2,16 +2,18 @@ import numpy as np
 from scipy.integrate import solve_ivp
 from scipy.optimize import minimize
 
+# Datos del problema
+m = 9500  # Masa en kg (9.5 toneladas)
+c_initial_guess = np.array([0.1])  # Valor inicial del coeficiente de amortiguamiento (ajústalo según sea necesario)
+k = 350e3  # Constante del resorte en N/m (350 N/mm)
+Fm = 1650  # Fuerza máxima en N
+xadm = 3.4 / 1000  # Desplazamiento admisible en metros (3.4 mm)
+P = 1.0351603978423491  # Periodo en segundos
+
+
 # Paso 1: Definir la ecuación diferencial
 def system(t, y, c):
-    # y[0] es la posición x, y[1] es la velocidad dx/dt
     x, v = y
-    k = 350  # Rigidez (N/m)
-    m = 9500  # Masa (kg)
-    Fm = 1650  # Fuerza máxima (N)
-    P = 1.0351603978423491  # Periodo (s)
-    
-    # Define la carga cíclica F(t) aquí (puede ser una función de t)
     F_t = Fm * np.sin(2 * np.pi * t / P)
 
     dxdt = v
@@ -19,21 +21,21 @@ def system(t, y, c):
 
     return [dxdt, dvdt]
 
-# Paso 2: Resolución de la ecuación diferencial
+
+# Paso 2: Definir la función para calcular el desplazamiento
+def displacement_difference(c):
+    sol = solve_ivp(system, t_span, initial_conditions, args=(c,), t_eval=np.linspace(*t_span, 1000))
+    x_calculated = sol.y[0]
+    return x_calculated[-1] - xadm  # Diferencia con el desplazamiento deseado
+
+
+# Paso 3: Resolución de la ecuación diferencial
 t_span = (0, 10)  # Rango de tiempo
 initial_conditions = [0, 0]  # Condición inicial [x0, v0]
 
-# Definir una función que calcule el desplazamiento en función de c
-def displacement(c):
-    sol = solve_ivp(system, t_span, initial_conditions, args=(c,), t_eval=np.linspace(*t_span, 1000))
-    return sol.y[0][-1]
+# Paso 4: Encontrar el valor óptimo de c
+result = minimize(displacement_difference, c_initial_guess, method='Nelder-Mead', tol=1e-6)
 
-# Paso 3: Encontrar el valor de c
-xadm = 0.0034  # Desplazamiento admisible (3.4 mm en metros)
-c_initial_guess = 0.1  # Adivinanza inicial para c
-
-result = minimize(lambda c: (displacement(c) - xadm) ** 2, c_initial_guess, method='Nelder-Mead', tol=1e-6)
-
-# Paso 4: Resultado
+# Paso 5: Resultado
 optimal_c = result.x[0]
 print(f"El coeficiente de amortiguamiento óptimo es: {optimal_c:.3f}")
