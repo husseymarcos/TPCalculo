5654fa3012aaa3ad5d8b832bc1013c3327ac26d0
husseymarcos <mhussey@mail.austral.edu.ar>
2023-11-07 20:56:22 -0300

    Update pvi.py
    outer loop para ciclos

--- pvi.py
+++ pvi.py
@@ -12,28 +12,37 @@ xadm = 3.4 / 1000  # Desplazamiento admisible en metros (3.4 mm)
 P = 1.0351603978423491  # Periodo en segundos
 
 
-def euler_mejorado(diff_eq: Callable[[float, float], float], initial_conds: List[Tuple[float, float]], h: float,
-                   timesteps: int) -> List[Tuple[float, float]]:
-    iteration = 0
-    solutions = [initial_conds[iteration]]
-    cycles_per_period = 6.069769787  # Número de ciclos deseados por período
+def euler_mejorado(diff_eq: Callable[[float, float], float], initial_conds: List[Tuple[float, float]], h: float, desired_cycles: float) -> List[Tuple[float, float]]:
+    solutions = [initial_conds[0]]  # Inicializa la lista de soluciones
     P = 1.0351603978423491  # Período en segundos
 
-    # Calcula el número de ciclos en el período deseado
-    total_cycles = cycles_per_period * P
+    # Calcula el número total de ciclos necesarios
+    total_cycles = 2 * math.pi / P
 
-    # Calcula el paso de tiempo necesario para al menos 500 puntos por ciclo
-    h = P / (total_cycles * 500)
+    # Inicializa las variables de ciclo y tiempo
+    current_cycles = 0
+    current_time = 0
 
-    timesteps = int(total_cycles * 500)  # Ajusta el número de pasos para alcanzar el tiempo deseado
+    # Bucle exterior: verifica si hemos alcanzado la cantidad deseada de ciclos
+    while current_cycles < total_cycles:
+        # Inicializa las condiciones iniciales para este ciclo
+        initial_conditions = solutions[-1]
+        iteration = 0
 
-    while iteration < timesteps:
-        conds = initial_conds[iteration]
-        x_incremented = conds[0] + h
-        f1 = h * diff_eq(conds[0], conds[1])
-        f2 = h * diff_eq(x_incremented, conds[1] + f1)
-        yf = conds[1] + (1 / 2 * (f1 + f2))
-        solutions.append((x_incremented, yf))
-        initial_conds.append((x_incremented, yf))
-        iteration += 1
+        # Bucle interior: realiza la integración para un ciclo
+        while current_time < P:
+            conds = initial_conditions
+            x_incremented = conds[0] + h
+            f1 = h * diff_eq(conds[0], conds[1])
+            f2 = h * diff_eq(x_incremented, conds[1] + f1)
+            yf = conds[1] + (1 / 2 * (f1 + f2))
+            solutions.append((x_incremented, yf))
+            initial_conditions = (x_incremented, yf)
+            current_time += h
+            iteration += 1
+
+        # Actualiza el número de ciclos completados
+        current_cycles += 1
+        current_time = 0
+
     return solutions
