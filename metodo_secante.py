0250929d5001713556a3f5d9486f8ad0fb8994d9
husseymarcos <mhussey@mail.austral.edu.ar>
2023-11-07 20:42:33 -0300

    ciclos y periodos para 500 puntos por ciclo

--- metodo_secante.py
+++ metodo_secante.py
@@ -1,10 +1,11 @@
 from typing import Callable
 import math
 
-def metodo_secante(f: Callable[[float], float],x0: float, x1: float ,err: float):
+
+def metodo_secante(f: Callable[[float], float], x0: float, x1: float, err: float):
     if abs(x1 - x0) < err: return x1
     print([x0, x1])
-    prox_x = x1 - ( (f(x1) * (x0 - x1)) / (f(x0) - f(x1)) )
+    prox_x = x1 - ((f(x1) * (x0 - x1)) / (f(x0) - f(x1)))
     return metodo_secante(f, x1, prox_x, err)
 
 
--- pvi.py
+++ pvi.py
@@ -12,29 +12,28 @@ xadm = 3.4 / 1000  # Desplazamiento admisible en metros (3.4 mm)
 P = 1.0351603978423491  # Periodo en segundos
 
 
-# timesteps tiene que ser > 500
-def euler_modificado(differential_equation, initial_condition, timesteps, h):
-    solutions = [initial_condition[0]]  # Inicializa la lista de soluciones con el valor inicial
-    y = initial_condition[0]  # Valor inicial
-    for t in timesteps:
-        f1 = differential_equation(t, y)
-        f2 = differential_equation(t + h, y + h * f1)
-        y = y + (h / 2) * (f1 + f2)
-        solutions.append(y)
-    return solutions
-
-
-def euler_mejorado(diff_eq: Callable[[float, float], float], initial_conds: List[Tuple[float, float]], h: float, timesteps: int) -> List[Tuple[float, float]]:
+def euler_mejorado(diff_eq: Callable[[float, float], float], initial_conds: List[Tuple[float, float]], h: float,
+                   timesteps: int) -> List[Tuple[float, float]]:
     iteration = 0
     solutions = [initial_conds[iteration]]
+    cycles_per_period = 6.069769787  # Número de ciclos deseados por período
+    P = 1.0351603978423491  # Período en segundos
+
+    # Calcula el número de ciclos en el período deseado
+    total_cycles = cycles_per_period * P
+
+    # Calcula el paso de tiempo necesario para al menos 500 puntos por ciclo
+    h = P / (total_cycles * 500)
+
+    timesteps = int(total_cycles * 500)  # Ajusta el número de pasos para alcanzar el tiempo deseado
+
     while iteration < timesteps:
         conds = initial_conds[iteration]
         x_incremented = conds[0] + h
         f1 = h * diff_eq(conds[0], conds[1])
         f2 = h * diff_eq(x_incremented, conds[1] + f1)
-        yf = conds[1] + (1/2 * (f1 + f2))
+        yf = conds[1] + (1 / 2 * (f1 + f2))
         solutions.append((x_incremented, yf))
         initial_conds.append((x_incremented, yf))
         iteration += 1
     return solutions
-
