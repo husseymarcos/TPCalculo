f18d6c110a91460263ff8f7fb7bf2cc665f05232
lucasmonteverdi1 <128722688+lucasmonteverdi1@users.noreply.github.com>
2023-11-08 17:57:55 -0300

    metodo secante sin el ZeroDivisionError

--- metodo_secante.py
+++ metodo_secante.py
@@ -17,15 +17,10 @@ def secante_con_validacion(f, x0, x1, err):
 
 
 def metodo_secante(f: Callable[[float], float], x0: float, x1: float, err: float):
-    #if abs(x1 - x0) < err: return x1
-    if (abs(f(x1) - f(3.4 / 1000))) < err: return x1
+    if (abs(x1 - x0)) < err: return x1
     print([x0, x1])
     prox_x = x1 - ((f(x1) * (x0 - x1)) / (f(x0) - f(x1)))
     return metodo_secante(f, x1, prox_x, err)
-    #f0 = f(x0)
-    #f1 = f(x1)
-    #next_x = x1 - f1 * (x0 - x1) / (f0 - f1)
-    #return metodo_secante(f, x1, next_x, err)
 
 
 if __name__ == '__main__':
