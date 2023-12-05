151fbe8e9812f1d9b2721cb353f83846f268e51e
husseymarcos <mhussey@mail.austral.edu.ar>
2023-12-04 22:28:03 -0300

    xmax/10 & tests para c = 30

--- datos_clase.py
+++ datos_clase.py
@@ -1,6 +1,3 @@
-from matplotlib import pyplot as plt
-import numpy as np
-
 from metodo_secante import metodo_secante as ms
 
 # Datos del problema
@@ -10,18 +7,15 @@ Fm = 1200  # Fuerza máxima en N
 xadm = 1.5 / 1000  # Desplazamiento admisible en metros (3.4 mm)
 P = 0.8111  # Periodo en segundos
 tolerancia = 1e-3  # Tolerancia para el método de la secante
-initial_conditions = [(0, 0)]  # Pares de valores (x;y)
 num_puntos_por_ciclo = 500
 h1 = P / num_puntos_por_ciclo
 
 
 def ec_dif(c):
     def F(t):
-        # Encuentra el número de ciclos completos que han pasado
-        num_ciclos_completos = int(t // P)
 
         # Encuentra el tiempo relativo dentro del ciclo actual
-        t_rel = t - (num_ciclos_completos * P)
+        t_rel = t - (int(t // P) * P)
 
         p_cicl = 0.1 * P
 
@@ -39,15 +33,13 @@ def ec_dif(c):
 
 def Euler(f, x0, y0, h, n):
     x = []
-    y = [] # y = velocidad
+    y = []  # y = velocidad
     x.append(x0)
     y.append(y0)
 
-    t = x0
-
     for i in range(1, n):
         t = h * (i - 1)
-        x.append(x[i-1] + y[i-1] * h)
+        x.append(x[i - 1] + y[i - 1] * h)
         y.append(y[i - 1] + h * f(x[i - 1], y[i - 1], t))
 
     return [x, y]
@@ -56,16 +48,8 @@ def Euler(f, x0, y0, h, n):
 # Función para calcular la amplitud deseada en función de "c"
 def f(c):
     solution = Euler(ec_dif(c), 0, 0, h1, num_puntos_por_ciclo * 10)
-
-    #tiempo_arreglo = np.arange(0, 10 * P, P / 500)
-    #plt.plot(tiempo_arreglo, solution[0], label='Solución Euler')
-    #plt.xlabel('x)')
-    #plt.ylabel('y')
-    #plt.title('Solución de la Ecuación Diferencial: y en función de x')
-    #plt.legend()
-    #plt.show()
     xmax = max(solution[0])
-    return xmax
+    return xmax / 10
 
 
 # Función a usar con el método de la secante.
@@ -74,8 +58,8 @@ g = lambda c: f(c) - xadm
 if __name__ == '__main__':
     try:
         # Valores iniciales para X0 y X1
-        X0 = 10
-        X1 = 20
+        X0 = 20
+        X1 = 30
 
         # Calcula el coeficiente de amortiguamiento "c" que evita vibraciones excesivas
         coeficiente_amortiguamiento = ms(g, X0, X1, tolerancia)
@@ -87,7 +71,16 @@ if __name__ == '__main__':
         amplitud_deseada = f(coeficiente_amortiguamiento)
         print(f"La amplitud deseada con el coeficiente de amortiguamiento obtenido es: {amplitud_deseada:.4f}")
 
+        # Verifica para c = 30
+        c = 30
+        solucion_c_30 = Euler(ec_dif(c), 0, 0, h1, num_puntos_por_ciclo * 10)
+        xmax_c_30 = max(solucion_c_30[0])
+        g_c_30 = g(c)
+
+        print()
+        print(f"Para c = 30:")
+        print(f"xmax calculado: {xmax_c_30:.5f}")
+        print(f"g(c) calculado: {g_c_30:.5f}")
+
     except Exception as e:
         print(f"Se produjo un error: {e}")
-
-
